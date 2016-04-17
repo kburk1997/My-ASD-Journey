d0dde998f1e6acac115a492c57d2510429ffc749
kburk1997 <kburk97@optonline.net>
2016-04-16 20:31:12 -0400

    refactor some code

--- App/MyASDJourney.codea/Main.lua
+++ App/MyASDJourney.codea/Main.lua
@@ -5,6 +5,46 @@ function setup()
     stage = 0
 end
 
+--Austin make your game here
+function game1()
+
+end
+
+--In this game, the user must select the right answer to "what color is this apple?"
+    --while there is a lot of background noise and everything is blurred
+function game2()
+    fontSize(20)
+    text("What color is this apple?", WIDTH/2, HEIGHT/2)
+
+    --Make four rectangles  
+    rect(WIDTH/30,HEIGHT/30, (WIDTH/2)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
+
+    --Top left rectangle
+    rect(WIDTH/30,(HEIGHT/6)+(HEIGHT/30), (WIDTH/2)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
+
+    --Bottom Right
+    rect(WIDTH/2,HEIGHT/30, (WIDTH)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
+
+    --Top right
+    rect(WIDTH/2,(HEIGHT/6)+(HEIGHT/30), (WIDTH)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
+
+    --Randomly select right answer
+    --0|1
+    --2|3
+
+    right_answer_position = math.random(0,3)
+
+    if right_answer_position == 0 then
+
+    elseif right_answer_position == 1 then
+
+    elseif right_answer_position == 2 then
+
+    else
+
+    end
+end
+
 -- This function gets called once every frame
 function draw()
     -- This sets a dark background color
@@ -14,45 +54,12 @@ function draw()
         
         
     elseif stage == 1 then
+        game1()
         
     elseif stage == 2 then
         
         --Katie make a mini game here
-
-        --User must pick correct answer in conversation
-        	--Sensory overload - a lot of background noise 
-    	fontSize(20)
-    	text("What color is this apple?", WIDTH/2, HEIGHT/2)
-
-    	--Make four rectangles  
-    	rect(WIDTH/30,HEIGHT/30, (WIDTH/2)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
-
-    	--Top left rectangle
-    	rect(WIDTH/30,(HEIGHT/6)+(HEIGHT/30), (WIDTH/2)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
-
-    	--Bottom Right
-    	rect(WIDTH/2,HEIGHT/30, (WIDTH)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
-
-    	--Top right
-    	rect(WIDTH/2,(HEIGHT/6)+(HEIGHT/30), (WIDTH)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
-
-        --Randomly select right answer
-        --0|1
-        --2|3
-
-        right_answer_position = math.random(0,3)
-
-        if right_answer_position == 0 then
-
-        elseif right_answer_position == 1 then
-
-        elseif right_answer_position == 2 then
-
-        else
-
-        end
-
-
+        game2()
     end
     
 end
