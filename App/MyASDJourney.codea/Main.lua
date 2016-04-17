ac533a9ec2eb91cc3d732f7d05fb5b9592bf9451
kburk1997 <kburk97@optonline.net>
2016-04-16 20:36:14 -0400

    more refactoring

--- App/MyASDJourney.codea/Main.lua
+++ App/MyASDJourney.codea/Main.lua
@@ -17,16 +17,7 @@ function game2()
     text("What color is this apple?", WIDTH/2, HEIGHT/2)
 
     --Make four rectangles  
-    rect(WIDTH/30,HEIGHT/30, (WIDTH/2)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
-
-    --Top left rectangle
-    rect(WIDTH/30,(HEIGHT/6)+(HEIGHT/30), (WIDTH/2)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
-
-    --Bottom Right
-    rect(WIDTH/2,HEIGHT/30, (WIDTH)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
-
-    --Top right
-    rect(WIDTH/2,(HEIGHT/6)+(HEIGHT/30), (WIDTH)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
+    draw_game2_rectangles()
 
     --Randomly select right answer
     --0|1
@@ -45,6 +36,21 @@ function game2()
     end
 end
 
+function draw_game2_rectangles()
+    
+    --Bottom left rectangleS
+    rect(WIDTH/30,HEIGHT/30, (WIDTH/2)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
+
+    --Top left rectangle
+    rect(WIDTH/30,(HEIGHT/6)+(HEIGHT/30), (WIDTH/2)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
+
+    --Bottom Right
+    rect(WIDTH/2,HEIGHT/30, (WIDTH)-(WIDTH/15), (HEIGHT/6)-(HEIGHT/30))
+
+    --Top right
+    rect(WIDTH/2,(HEIGHT/6)+(HEIGHT/30), (WIDTH)-(WIDTH/15), (HEIGHT/4)-(1.75*HEIGHT/15))
+end
+
 -- This function gets called once every frame
 function draw()
     -- This sets a dark background color
@@ -55,7 +61,7 @@ function draw()
         
     elseif stage == 1 then
         game1()
-        
+
     elseif stage == 2 then
         
         --Katie make a mini game here
